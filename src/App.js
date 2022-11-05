838009e9815d1c298182f2cde59542b006537fc6
sevvalkolemen <seval.kolemen@gmail.com>
2022-11-05 14:48:04 +0300

    completed login pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Toaster } from "react-hot-toast";
 import { Routes, Route } from "react-router-dom";
 import  Home  from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <Toaster position="top-right" />
+      <Toaster position="bottom-right" />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </>
   );
---
+++ src/pages/Login.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState } from "react";
+import { login } from "../firebase";
+import Navbar from "../component/Navbar";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = await login(email, password);
+    console.log(user);
+  };
+  return (
+    <>
+      <Navbar />
+      <div
+        className="modal modal-signin position-static d-block py-5"
+        tabIndex="-1"
+        role="dialog"
+        id="modalSignin"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content rounded-4 shadow">
+            <div className="modal-header p-5 pb-4 border-bottom-0">
+              <h1 className="fw-bold mb-0 fs-2">Log in</h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+
+            <div className="modal-body p-5 pt-0">
+              <form onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control rounded-3"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input
+                    type="password"
+                    className="form-control rounded-3"
+                    id="floatingPassword"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <button
+                  className="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
+                  type="submit"
+                  disabled={!email || !password}
+                >
+                  Login
+                </button>
+                <a href="#">
+                  Forgot Password ?
+                </a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
