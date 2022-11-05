f5803bec660214f93974f448fd6335082634bc93
sevvalkolemen <seval.kolemen@gmail.com>
2022-11-05 14:42:24 +0300

    completed register pages

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { register } from "../firebase";
+import Navbar from "../component/Navbar";
 
 function Register() {
   const [email, setEmail] = useState("");
@@ -12,27 +13,66 @@ function Register() {
     console.log(user);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Eposta"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        className="btn btn-primary"
-        type="submit"
-        disabled={!email || !password}
+    <>
+      <Navbar />
+      <div
+        className="modal modal-signin position-static d-block py-5"
+        tabIndex="-1"
+        role="dialog"
+        id="modalSignin"
       >
-        Kayıt
-      </button>
-    </form>
+        <div className="modal-dialog" role="document">
+          <div className="modal-content rounded-4 shadow">
+            <div className="modal-header p-5 pb-4 border-bottom-0">
+              <h1 className="fw-bold mb-0 fs-2">Sign up</h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+
+            <div className="modal-body p-5 pt-0">
+              <form onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control rounded-3"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input
+                    type="password"
+                    className="form-control rounded-3"
+                    id="floatingPassword"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <button
+                  className="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
+                  type="submit"
+                  disabled={!email || !password}
+                >
+                  Sign up
+                </button>
+                <small className="text-muted">
+                  By clicking Sign up, you agree to the terms of use.
+                </small>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
