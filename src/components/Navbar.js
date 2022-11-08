45a04bd20bba7766fe7649a265da9ab33489bb94
sevvalkolemen <seval.kolemen@gmail.com>
2022-11-08 15:41:55 +0300

    changed the visibility of auth buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../firebase";
+import { logout as logoutHandle } from "../store/auth";
 
 function Navbar() {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    await logout();
+    dispatch(logoutHandle());
+    navigate("/login", {
+      replace: true,
+    });
+  };
+
   return (
     <header className="p-3 text-bg-dark">
       <div className="container">
@@ -12,7 +27,7 @@ function Navbar() {
             to="/"
             className="d-flex align-items-center mb-2 mb-lg-0 me-5 text-white text-decoration-none"
           >
-            <FontAwesomeIcon icon={faCode} className="fa-2xl"/>
+            <FontAwesomeIcon icon={faCode} className="fa-2xl" />
           </Link>
 
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -22,7 +37,10 @@ function Navbar() {
               </Link>
             </li>
             <li>
-              <Link to="/whatismentorship" className="nav-link px-2 text-white me-2">
+              <Link
+                to="/whatismentorship"
+                className="nav-link px-2 text-white me-2"
+              >
                 What is Mentorship
               </Link>
             </li>
@@ -43,14 +61,22 @@ function Navbar() {
             </li>
           </ul>
 
-          <div className="text-end">
-            <Link to="/login"  className="btn btn-outline-light me-2">
-              Login
-            </Link>
-            <Link to="/register" className="btn btn-warning">
-              Sign-up
-            </Link>
-          </div>
+          {!user ? (
+            <div className="text-end">
+              <Link to="/login" className="btn btn-outline-light me-2">
+                Login
+              </Link>
+              <Link to="/register" className="btn btn-warning">
+                Sign-up
+              </Link>
+            </div>
+          ) : (
+            <div className="text-end">
+              <button onClick={handleLogout} className="btn btn-danger">
+                Log out
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </header>
